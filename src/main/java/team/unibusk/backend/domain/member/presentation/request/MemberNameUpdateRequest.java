817beaa9d78da7040eb602c72e8df30846f45d00
y22jun <tlsdpwns0612@naver.com>
2026-01-11 00:07:50 +0900

    Feat: 길이 제한 조건 추가

--- src/main/java/team/unibusk/backend/domain/member/presentation/request/MemberNameUpdateRequest.java
+++ src/main/java/team/unibusk/backend/domain/member/presentation/request/MemberNameUpdateRequest.java
@@ -1,6 +1,7 @@
 package team.unibusk.backend.domain.member.presentation.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import team.unibusk.backend.domain.member.application.dto.request.MemberNameUpdateServiceRequest;
 
@@ -8,6 +9,7 @@ import team.unibusk.backend.domain.member.application.dto.request.MemberNameUpda
 public record MemberNameUpdateRequest(
 
         @NotBlank(message = "이름을 입력해 주세요.")
+        @Size(max = 10, message = "이름은 최대 10글자까지 작성 가능합니다.")
         String name
 
 ) {
